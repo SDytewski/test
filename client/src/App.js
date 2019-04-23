f314acc63aa9d4791d934bb9d2fb28535ff71c2c
Steven Dytewski <40008730+Dy2ski@users.noreply.github.com>
2019-04-23 11:03:46 -0700

    repository

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+
+      <div>
+        <Nav />
+        <Switch>
+          <Route path="/" component={Books} />
+
+          <Route path="/books" component={Books} />
+          <Route path="/books/:id" component={Detauk} />
+          <Route component={NoMatch} />
+
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
